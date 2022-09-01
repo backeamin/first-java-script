d9931286d66869c106bd9454c8d5dfed75d95b20
deveamin <eaminshikder2003@gmail.com>
2022-09-01 14:21:49 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function nightMode(){
+    document.body.style.color = 'white';
+    document.body.style.backgroundColor = 'black';
+}
+function dayMode(){
+    document.body.style.color = 'black';
+    document.body.style.backgroundColor = 'white';
+}
